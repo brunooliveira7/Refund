2d31044271c2b65e149877da3f444513bc646582
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-28 20:58:19 -0300

    Format the currency

--- scripts.js
+++ scripts.js
@@ -4,8 +4,21 @@ const amount = document.getElementById("amount");
 //capta quando entra conteúdo no input
 amount.oninput = () => {
   //captura o valor do input e substitui valor não numérico por vazio
-  let value = amount.value.replace(/\D/g, "")
-  //não deixa aparecer o valor que não é numero
-  amount.value = value;
-  
+  let value = amount.value.replace(/\D/g, "");
+
+  //transforma o valor para centavos
+  value = Number(value) / 100;
+
+  //não deixa aparecer os valores não numérico - atualiza
+  amount.value = formatCurrencyBRL(value);
 };
+
+//formata o que foi digitado para o padrão BRL
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  //retorna o valor formatado
+  return value;
+}
