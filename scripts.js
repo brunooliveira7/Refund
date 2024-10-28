0e85fd240f921728fc67d30be57d47b7b6f5b029
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-28 17:25:55 -0300

    capture input event

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//seleciona os elementos do formulário
+const amount = document.getElementById("amount");
+
+//capta quando entra conteúdo no input
+amount.oninput = () => {
+  console.log("conteúdo");
+};
